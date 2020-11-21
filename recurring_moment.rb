36cf5848ca54a630f0326616eaffe508bb9f9063
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-11-21 13:37:47 -0500

    Code debugged and able to match correct input dates.

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -11,15 +11,22 @@ class RecurringMoment
   def match(date)
     current = @start
 
-    while current < date
+    while current <= date  
       if current == date
         return true
       end
 
       if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
         current = current.advance(months: @interval)
+        
+        if date == date.end_of_month.beginning_of_day
+          current = current.end_of_month.beginning_of_day
+        elsif date == date.end_of_month.yesterday.beginning_of_day
+          current = current.end_of_month.yesterday.beginning_of_day
+        end
+
+      elsif @period == 'weekly'
+        current = current.advance(weeks: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
       end
@@ -28,3 +35,10 @@ class RecurringMoment
     return false
   end
 end
+
+
+my_date = DateTime.parse("2018-01-31").advance(months: 1)
+p my_date.advance(months: 1)
+p DateTime.parse("2018-01-31").end_of_month.advance(months: 3)
+
+
